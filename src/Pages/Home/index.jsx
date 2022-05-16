279055ca517d3a5190366b2f15b1825bc8410687
babaquero07 <brayanalexanderb@gmail.com>
2022-05-16 12:08:19 -0500

    Estructura terminada al 80 %

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import SearchBar from "../../Components/SearchBar";
+import Button from "../../Components/Button";
+import style from "./HomePage.module.css";
+import { gql, useQuery } from "@apollo/client";
+
+const countriesList = gql`
+  {
+    countries {
+      emoji
+      name
+      capital
+      currency
+      phone
+      continent {
+        name
+      }
+      languages {
+        name
+      }
+    }
+  }
+`;
+
+const HomePage = () => {
+  const { data, loading } = useQuery(countriesList);
+  const [countries, setCountries] = useState({});
+
+  useEffect(() => {
+    setCountries(data);
+  }, [data]);
+
+  if (loading) return <h1>Loading...</h1>;
+
+  return (
+    <div className={style.homeContainer}>
+      <div className={style.homeContainer__content}>
+        <h1>Country search</h1>
+        <SearchBar />
+        <div>
+          <h2>Group by: </h2>
+          <Button text="Continent" />
+          <Button text="Language" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
